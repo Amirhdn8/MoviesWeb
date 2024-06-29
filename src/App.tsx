d802d754f902dc636fd87e09b50192f537fe81d7
Amirhdn8 <amihoseinhdn2020@gmail.com>
2024-06-29 15:49:25 +0330

    create project and install chakra

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Button, ButtonGroup } from '@chakra-ui/react'
+
+const App = () => {
+  return (
+    <Button colorScheme='blue'>Button</Button>
+  )
+}
+
+export default App
