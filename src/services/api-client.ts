ea384341b49bc20303c02eca5e273b59c1535dca
Amirhdn8 <amihoseinhdn2020@gmail.com>
2024-10-13 11:32:02 +0330

    Fetch game details

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -25,6 +25,12 @@ class APIClient<T> {
       .get<FetchResponse<T>>(this.endpont, config)
       .then((res) => res.data);
   };
+
+  get = (id: number | string) => {
+    return axiosInstance
+      .get<T>(this.endpont + "/" + id)
+      .then((res) => res.data);
+  };
 }
 
 export default APIClient;
